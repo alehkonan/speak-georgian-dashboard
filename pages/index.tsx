f6d52fa224900af1e4c54e3919b9dc538eb6d890
Oleg Konan <o.konan@softteco.com>
2022-09-04 17:29:06 +0400

    init

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Speak Georgian dashboard</h1>
+    </div>
+  );
+};
+
+export default Home;
