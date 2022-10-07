59589f603ca1d2cd515191c8f69b10a9f1c23a69
aleh konan <olegkonanff@gmail.com>
2022-10-07 17:27:47 +0400

    added next-connect

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -1,24 +1,18 @@
 import { connectDatabase } from 'database';
 import { History } from 'database/models';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import type { Word } from 'types';
+import nextConnect from 'next-connect';
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse<Word[] | string>,
-) => {
-  if (req.method === 'GET') {
-    try {
-      await connectDatabase();
-      const words = await History.find();
-      res.status(200).json(words);
-    } catch (error) {
-      if (error instanceof Error) {
-        console.log('cached error');
-        res.status(500).json(error.message);
-      }
-    }
-  }
-};
+const handler = nextConnect<NextApiRequest, NextApiResponse>({
+  onError: (error, req, res) => {
+    res.status(500).json({ message: error.message });
+  },
+});
+
+handler.get(async (req, res) => {
+  await connectDatabase();
+  const words = await History.find();
+  res.status(200).json(words);
+});
 
 export default handler;
